95131e48a8952260708cc582128b34274bc3c36f
Peter gimbap <kjh22c@hanmail.net>
2017-06-01 15:42:41 -0700

    for 172 and error

--- routes/perf.js
+++ routes/perf.js
@@ -19,7 +19,11 @@ var metar_data
 	,Perf152_data_TO_Gnd_Roll
 	,Perf152_data_TO_50_Clr
 	,Perf152_data_Land_Gnd_Roll
-	,Perf152_data_Rate_Of_Climb;
+	,Perf152_data_Rate_Of_Climb
+	,Perf172_data_TO_Gnd_Roll
+	,Perf172_data_TO_50_Clr
+	,Perf172_data_Land_Gnd_Roll
+	,Perf172_data_Rate_Of_Climb;
 
 exports.index = function(req, res){
 	readData(function(){
@@ -34,6 +38,10 @@ exports.index = function(req, res){
 		  					,Perf152_data_TO_50_Clr : Perf152_data_TO_50_Clr
 		  					,Perf152_data_Land_Gnd_Roll :Perf152_data_Land_Gnd_Roll
 		  					,Perf152_data_Rate_Of_Climb :Perf152_data_Rate_Of_Climb
+		  					,Perf172_data_TO_Gnd_Roll : Perf172_data_TO_Gnd_Roll  
+		  					,Perf172_data_TO_50_Clr : Perf172_data_TO_50_Clr
+		  					,Perf172_data_Land_Gnd_Roll :Perf172_data_Land_Gnd_Roll
+		  					,Perf172_data_Rate_Of_Climb :Perf172_data_Rate_Of_Climb
 		  					
 		  						}  );
 	});
@@ -44,7 +52,7 @@ var readData = function(callback) {
 	//METAR 
 	var url = { 
 			url : 
-				"http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=KHIO&hoursBeforeNow=1"
+				"http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=KHIO&hoursBeforeNow=2"
 			,json: false
 		};
 	
@@ -93,33 +101,60 @@ var readData = function(callback) {
 				//
 				// Load the data
 				// forcast function
-				function Cur152TOGndRoll(cur_temp,cur_pressure){}
-				function Cur152TO50Clr(cur_temp,cur_pressure){}
-				function Cur152LandRoll(cur_temp,cur_pressure){}
-				function Cur152RateOfClimb(cur_temp,cur_pressure){}
+				function CurTOGndRoll(cur_temp,cur_pressure, data){}
+				function CurTO50Clr(cur_temp,cur_pressure, data){}
+				function CurLandRoll(cur_temp,cur_pressure, data){}
+				function CurRateOfClimb(cur_temp,cur_pressure, data){}
 				
 				
-				//data		
-				var takeoffGndRoll = [[640,695,755,810] // p. alti 0 temp 0~30
+				//data 152		
+				var takeoffGndRoll_152 = [[640,695,755,810] // p. alti 0 temp 0~30
 						,[705,765,825,890]				// p. alti 1000 temp 0~30
 						,[775,840,910,980] ];			// p. alti 2000 temp 0~30
 			
-				var takeoff50ft = [
+				var takeoff50ft_152 = [
 					[1190,	1290,	1390,	1495	]  // p. alti 0 temp 0~30
 					,[1310,	1420,	1530,	1645]  // p. alti 1000 temp 0~30
 					,[1445,	1565,	1690,	1820]];// p. alti 2000 temp 0~30
 			
-				var rateOfClimb = [
+				var rateOfClimb_152 = [
 					 [835,	765	,700,	630] // p.alti 0 temp -20 to +40
 					 ,[735,	670,600,	535] // p.alti 2000 temp -20 to +40
 					 ];
 			
-				var landGndRoll = [
+				var landGndRoll_152 = [
 					[	450	,	465	,	485	,	500]
 					,[	465	,	485	,	500	,	520]
 					,[	485	,	500	,	520	,	535]
 				       ];
 			
+				//data 172		
+				var takeoffGndRoll_172 = 
+					[
+						[	795	,	860	,	925	,	995	]
+						,[	875	,	940	,	1015	,	1090	]
+						,[	960	,	1035	,	1115	,	1200	]
+						];			// p. alti 2000 temp 0~30
+			
+				var takeoff50ft_172 = [
+					[	1446	,	1570	,	1685	,	1810	]
+					,[	1605	,	1725	,	1860	,	2000	]
+					,[	1770	,	1910	,	2060	,	2220	]
+					];// p. alti 2000 temp 0~30
+			
+				var rateOfClimb_172 = [
+					[	805	,	745	,	685	,	625	]
+					,[	695	,	640	,	580	,	525	]
+					 ];
+			
+				var landGndRoll_172 = [
+					[	510	,	530	,	550	,	570	]
+					,[	530	,	550	,	570	,	590	]
+					,[	550	,	570	,	590	,	610	]
+
+				       ];
+			
+				
 				// for cal unit 게산용 unit
 				var tempDegreeby10 = [0,10,20,30];
 				var tempDegreeby20 = [-20,0,20,40];
@@ -127,12 +162,18 @@ var readData = function(callback) {
 				var pressureDegreeBy2000 = [0,2000];
 				
 				//
-				// 데이터 저장
-				Perf152_data_TO_Gnd_Roll = Cur152TOGndRoll(metar_temp,pa).toFixed(1);
-				Perf152_data_TO_50_Clr = Cur152TO50Clr(metar_temp,pa).toFixed(1);
-				Perf152_data_Land_Gnd_Roll = Cur152LandRoll(metar_temp,pa).toFixed(1);
-				Perf152_data_Rate_Of_Climb = Cur152RateOfClimb(metar_temp,pa).toFixed(1);
+				// 데이터 저장 152
+				Perf152_data_TO_Gnd_Roll = CurTOGndRoll(metar_temp,pa,takeoffGndRoll_152).toFixed(1);
+				Perf152_data_TO_50_Clr = CurTO50Clr(metar_temp,pa,takeoff50ft_152).toFixed(1);
+				Perf152_data_Land_Gnd_Roll = CurLandRoll(metar_temp,pa,landGndRoll_152).toFixed(1);
+				Perf152_data_Rate_Of_Climb = CurRateOfClimb(metar_temp,pa,rateOfClimb_152).toFixed(1);
 				
+				// 데이터 저장 172
+				Perf172_data_TO_Gnd_Roll = CurTOGndRoll(metar_temp,pa,takeoffGndRoll_172).toFixed(1);
+				Perf172_data_TO_50_Clr = CurTO50Clr(metar_temp,pa,takeoff50ft_172).toFixed(1);
+				Perf172_data_Land_Gnd_Roll = CurLandRoll(metar_temp,pa,landGndRoll_172).toFixed(1);
+				Perf172_data_Rate_Of_Climb = CurRateOfClimb(metar_temp,pa,rateOfClimb_172).toFixed(1);
+
 				//callback();
 			
 				//Time Series Cal	
@@ -157,76 +198,53 @@ var readData = function(callback) {
 				}
 			
 			
-				// 가로축 세로축 모두 계산을 해야 하기떄문에 이렇게 함. 현재 온도를 넣고 , 프레셔 0,1000,2000,3000단위로 모두 구함 
-			
-				function Cur152TOGndRoll(cur_temp,cur_pressure){
-					//T.O Gnd Roll
-					var ToGndRollForecast = 
-							[
-						     Forecast(cur_temp, tempDegreeby10 ,takeoffGndRoll[0]) // 0 alt. pressure 0 일때
-						     , Forecast(cur_temp, tempDegreeby10 ,takeoffGndRoll[1]) // 0 alt. pressure 1000 일때
-						     , Forecast(cur_temp, tempDegreeby10 ,takeoffGndRoll[2]) // 0 alt. pressure 2000 일때
-						     ];
-			
-					var curToGndRoll =
-							Forecast(cur_pressure, pressureDegree, ToGndRollForecast  );
-			
-					return curToGndRoll ;
-				}
-			
-				function Cur152TO50Clr(cur_temp,cur_pressure){ 
-					//T.O 50ft 
-					var To50ftForecast =
-							[  Forecast(cur_temp, tempDegreeby10 ,takeoff50ft[0]) // 0 alt. pressure 0 일때
-						    , Forecast(cur_temp, tempDegreeby10 ,takeoff50ft[1]) // 0 alt. pressure 1000 일때
-						    , Forecast(cur_temp, tempDegreeby10 ,takeoff50ft[2]) // 0 alt. pressure 2000 일때
-						    ];
-			
-					var curTo50ft =
-							Forecast(cur_pressure, pressureDegree, To50ftForecast  );
-			
-					return curTo50ft ;
-				}
-			
-				//Land Roll
-				function Cur152LandRoll(cur_temp,cur_pressure){ 
+					// 가로축 세로축 모두 계산을 해야 하기떄문에 이렇게 함. 현재 온도를 넣고 , 프레셔 0,1000,2000,3000단위로 모두 구함 
+					function CurTOGndRoll(cur_temp,cur_pressure, takeoffGndRoll) //temp , pre , 데이터 
+					{
+						//T.O Gnd Roll
+						var ToGndRollForecast = new Array;
+						for(i=0;i<takeoffGndRoll.length;i++){
+							ToGndRollForecast[i] = Forecast(cur_temp, tempDegreeby10 ,takeoffGndRoll[i]); // 0 alt. pressure 0 ,1000,2000일때
+						}
+						var curToGndRoll =
+								Forecast(cur_pressure, pressureDegree, ToGndRollForecast  );
+						return curToGndRoll ;
+					}
+	
+					function CurTO50Clr(cur_temp,cur_pressure, takeoff50ft){ 
+						//T.O 50ft 
+						var To50ftForecast = new Array;
+						for(i=0;i<takeoff50ft.length;i++){
+							To50ftForecast[i]  = Forecast(cur_temp, tempDegreeby10 ,takeoff50ft[i]);
+						}
+						var curTo50ft =
+								Forecast(cur_pressure, pressureDegree, To50ftForecast  );
+						return curTo50ft ;
+					}
 					//Land Roll
-					var LandGndRollForecast =
-							[  Forecast(cur_temp, tempDegreeby10 ,landGndRoll[0]) // 0 alt. pressure 0 일때
-					         , Forecast(cur_temp, tempDegreeby10 ,landGndRoll[1]) // 0 alt. pressure 1000 일때
-					         , Forecast(cur_temp, tempDegreeby10 ,landGndRoll[2]) // 0 alt. pressure 2000 일때
-					     	];
-			
-					var curLandGndRoll =
-							Forecast(cur_pressure, pressureDegree, LandGndRollForecast  );
-					return curLandGndRoll;
-					
-				}
-			
-				//Climb Perf
-				function Cur152RateOfClimb(cur_temp,cur_pressure){ 
+					function CurLandRoll(cur_temp,cur_pressure, landGndRoll){ 
+						//Land Roll
+						var LandGndRollForecast = new Array;
+						for(i=0;i<landGndRoll.length;i++){
+							LandGndRollForecast[i] =   Forecast(cur_temp, tempDegreeby10 ,landGndRoll[i]);
+						}
+						var curLandGndRoll =
+								Forecast(cur_pressure, pressureDegree, LandGndRollForecast  );
+						return curLandGndRoll;
+					}
+	
 					//Climb Perf
-					var rateOfClimbForecast =
-							[  Forecast(cur_temp, tempDegreeby20 ,rateOfClimb[0]) // 0 alt. pressure 0 일때
-					         , Forecast(cur_temp, tempDegreeby20 ,rateOfClimb[1]) // 0 alt. pressure 2000 일때
-					     	];
-			
-					var curRateOfClimb =
-							Forecast(cur_pressure, pressureDegreeBy2000, rateOfClimbForecast  );
-					
-					return curRateOfClimb;
-					
-				}
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
+					function CurRateOfClimb(cur_temp,cur_pressure, rateOfClimb){ 
+						//Climb Perf
+						var rateOfClimbForecast = new Array;
+						for(i=0 ;i<rateOfClimb.length;i++){
+							rateOfClimbForecast[i] = Forecast(cur_temp, tempDegreeby20 ,rateOfClimb[i]) 
+						}
+						var curRateOfClimb =
+								Forecast(cur_pressure, pressureDegreeBy2000, rateOfClimbForecast);
+						return curRateOfClimb;
+						
+					}
 				
 				//
 				//
